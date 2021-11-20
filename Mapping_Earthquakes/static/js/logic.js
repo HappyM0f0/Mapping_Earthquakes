199cf0c026b6018ecf03d533e92780128bfa6fe3
Vynstan <HappyM0f0@icloud.com>
2021-11-20 08:34:16 -0600

    updates

--- Mapping_Earthquakes/static/js/logic.js
+++ Mapping_Earthquakes/static/js/logic.js
@@ -15,6 +15,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // ALTERNATIVE METHOD END
 
 //  Add a marker to the map for Los Angeles, California.
+//  this will creat a circle that is fixed throughout different zoom levels
 // let marker = L.circleMarker([34.0522, -118.2437], {
 //     radius: 300,
 //     color:'black',
@@ -22,7 +23,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //  }).addTo(map);
 
  let marker = L.circle([34.0522, -118.2437], {
-    radius: 3000,
+    radius: 300000,
     color:'black',
     fillColor:'#FFFFa1'
  }).addTo(map);
@@ -31,7 +32,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/dark-v10',
--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -15,6 +15,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // ALTERNATIVE METHOD END
 
 //  Add a marker to the map for Los Angeles, California.
+//  this will creat a circle that is fixed throughout different zoom levels
 // let marker = L.circleMarker([34.0522, -118.2437], {
 //     radius: 300,
 //     color:'black',
@@ -22,50 +23,36 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //  }).addTo(map);
 
 // let marker = L.circle([34.0522, -118.2437], {
-//     radius: 3000,
+//     radius: 300000,
 //     color:'black',
 //     fillColor:'#FFFFa1'
 //  }).addTo(map);
 
-// An array containing each city's location, state, and population.
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-  },
-  {
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-  },
-  {
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-  },
-  {
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-  },
-  {
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-  }
-  ];
+// Get data from cities.js
+let cityData = cities;
+
+
+// loop through cities to map all points
+// for (let i = 0; i < cities.length; i++); // alt way of doing it
+cities.forEach(function(city){
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: (city.population - 200000)/100000,
+    fillColor: 'orange',
+    color: 'orange',
+    weight: 4
+  })
+  // .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population "+ city.population.toLocaleString() + "</h3>")
+  .bindPopup(`<h2> ${city.city}, ${city.state} </h2> <hr> <h3>Population ${city.population.toLocaleString()} </h3>`) // toLocaleString() adds comma sperators
+  .addTo(map);
+});
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'dark-v10',
+    id: 'mapbox/dark-v10',
     // tileSize: 512,
     // zoomOffset: -1,
     accessToken: API_KEY
