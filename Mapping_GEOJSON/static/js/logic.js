a9059b98c9d6d02c68be5e504913ed1064c46e12
Vynstan <HappyM0f0@icloud.com>
2021-11-21 16:42:07 -0600

    Update logic.js

--- Mapping_GEOJSON/static/js/logic.js
+++ Mapping_GEOJSON/static/js/logic.js
@@ -30,15 +30,33 @@ let sanFranAirport =
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
 
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport).addTo(map);
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport).addTo(map);
 
+// Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup(`<h2>${feature.properties.name}</h2> <br>${feature.properties.city}, ${feature.properties.country}`);
+//   }
+
+// }).addTo(map);
+
+//grabing GeoJson data with onEachFeature to add data to all markers
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup(`<h2>Airport Code: ${feature.properties.faa}</h2> <hr> <h4>Airport Name: ${feature.properties.name}</h4>`);
+   }
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satellite-streets-v11',
+    id: 'mapbox/outdoors-v11',
     // tileSize: 512,
     // zoomOffset: -1,
     accessToken: API_KEY
