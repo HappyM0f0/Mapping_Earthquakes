284fb7135f15f9bafd2fd2edf46493c36b42c124
Vynstan <HappyM0f0@icloud.com>
2021-11-22 00:53:24 -0600

    Update challenge_logic.js

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -137,7 +137,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // 5.2 Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
     function getMajorColor(magnitude) {
       if (magnitude > 6) {
-        return "#ea2c2c";
+        return "#A93226";  //legend only provided 5+ colors
       }
       if (magnitude > 5) {
         return "#ea2c2c";
@@ -174,14 +174,15 @@ let legend = L.control({
 legend.onAdd = function() {
   let div = L.DomUtil.create("div", "info legend");
 
-  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const magnitudes = [0, 1, 2, 3, 4, 5, 6];
   const colors = [
     "#98ee00",
     "#d4ee00",
     "#eecc00",
     "#ee9c00",
     "#ea822c",
-    "#ea2c2c"
+    "#ea2c2c",
+    "#A93226"
   ];
 
 // Looping through our intervals to generate a label with a colored square for each interval.
